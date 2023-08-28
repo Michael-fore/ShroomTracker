7d55f0409d2283a3d7f32a2315160530558c698b
Michael <michael-fore@sbcglobal.net>
2023-08-28 12:16:13 -0600

    working shroom dropper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import GoogleMapReact from "google-map-react";
-import { useState } from 'react';
+import GoogleMap from 'google-maps-react-markers'
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { GiMushrooms } from "react-icons/gi";
 
 const onClick = ({
@@ -19,15 +19,48 @@ const onClick = ({
 }) => {
   //create markers
   setMarkers((markers) => [...markers, <GiMushrooms lat={lat} lng={lng} key={markers.length} 
-                                        className='h-12 w-12'/>]) 
+                                        className='h-2 w-2'/>]) 
   console.log(markers)
 };
+
+
+const Map = ({defaultProps,onGoogleApiLoaded,markers}) => {
+
+  return (
+    <div style={{ height: "50vh", width: "100%" }}>
+      <GoogleMap
+        id='map'
+        api={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        onGoogleApiLoaded={onGoogleApiLoaded}
+        
+      >
+        {markers}
+      </GoogleMap>
+    </div>
+  );
+}
 
 
 
 export default function Home() {
 
+  const mapRef = useRef(null)
   const [markers, setMarkers] = useState([])
+  const [mapReady, setMapReady] = useState(false)
+
+  const onGoogleApiLoaded = ({ map, maps }) => {
+    mapRef.current = map
+    setMapReady(true)
+    window.google.maps.event.addDomListener(mapRef.current, "click", (e) => {
+      console.log(e.latLng.lat(),e.latLng.lng())
+      const lat = e.latLng.lat()
+      const lng = e.latLng.lng()
+      setMarkers((markers) => [...markers, <GiMushrooms lat={lat} lng={lng} key={markers.length} 
+        className='h-12 w-12'/>]) 
+    })
+  }
 
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
@@ -39,27 +72,14 @@ export default function Home() {
     },
     zoom: 11,
   };
+  // https://stackoverflow.com/questions/9356724/google-map-api-zoom-range
+  // Zoom level 0 is the most zoomed out zoom level available and each integer step in zoom level halves the X and Y extents of the view and doubles the linear resolution.
+  // Google Maps was built on a 256x256 pixel tile system where zoom level 0 was a 256x256 pixel image of the whole earth. A 256x256 tile for zoom level 1 enlarges a 128x128 pixel region from zoom level 0.
+  
+  
+  // meters_per_pixel = 156543.03392 * Math.cos(latLng.lat() * Math.PI / 180) / Math.pow(2, zoom)
 
-  const Map = (m:any, setM:any ) => {
-   
-    console.log(markers)
-    console.log(defaultProps)
-    return (
-      <div style={{ height: "50vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-          onClick={(e,c) => {
-            console.log(e,c)
-            onClick({lat: e.lat, lng: e.lng, setMarkers, markers})
-          }}
-        >
-          {markers}
-        </GoogleMapReact>
-      </div>
-    );
-  }
+
 
   return (
     <>
@@ -69,7 +89,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Map markers={markers} setMarkers={setMarkers} />
+        <Map defaultProps={defaultProps} onGoogleApiLoaded={onGoogleApiLoaded} markers={markers}/>
         <GiMushrooms className='h-12 w-12'/>
       </main>
     </>
